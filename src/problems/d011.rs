a380ee64e36cb58710370fb2a5c5e8345ae98564
HakunaRumata <49195326+HakunaRumata@users.noreply.github.com>
2022-12-13 14:19:51 +0100

    d11 p1, p2 overflows on full runs

--- src/problems/d011.rs
+++ src/problems/d011.rs
@@ -1,8 +1,8 @@
-use std::cell::RefCell;
+use std::borrow::BorrowMut;
 
 pub struct Day {
-    p1: i64,
-    p2: i64,
+    p1: u128,
+    p2: u128,
     input: String,
 }
 
@@ -40,27 +40,45 @@ Monkey 3:
   Operation: new = old + 3
   Test: divisible by 17
     If true: throw to monkey 0
-    If false: throw to monkey 1"),
+    If false: throw to monkey 1")
         }
     }
 
     fn do_p1(&mut self) {
         let mut monkees: Vec<Monke> = Vec::new();
-        let mut monkees = RefCell::new(monkees);
-        for monke in  self.input.split("\n\n"){
-            monkees.borrow_mut().push(Monke::new(monke));
+        let mut all_items = Vec::new();
+        for monke in self.input.split("\n\n") {
+            monkees.push(Monke::new(monke, all_items.borrow_mut()));
         }
-
         for _i in 0..20 {
-            for monke in monkees.borrow().iter() {
-                if let Ok(monksters) = monkees.try_borrow_mut() {
-                    
-                }
+            for (j, monke) in &mut monkees.iter_mut().enumerate() {
+                monke.process_item(all_items.borrow_mut(), j, true);
             }
         }
+        monkees.sort_by(|a, b| a.inspections.partial_cmp(&b.inspections).unwrap());
+        monkees.reverse();
+        self.p1 = monkees[0].inspections * monkees[1].inspections;
     }
 
-    fn do_p2(&mut self) {}
+    fn do_p2(&mut self) {
+        let mut monkees: Vec<Monke> = Vec::new();
+        let mut all_items = Vec::new();
+        for monke in self.input.split("\n\n") {
+            monkees.push(Monke::new(monke, all_items.borrow_mut()));
+        }
+        // TODO: deal with overflow :)
+        for _i in 0..20 {
+            for (j, monke) in &mut monkees.iter_mut().enumerate() {
+                monke.process_item(all_items.borrow_mut(), j, false);
+            }
+        }
+        // for item in all_items {
+        //     println!("{:?}", item);
+        // }
+        monkees.sort_by(|a, b| a.inspections.partial_cmp(&b.inspections).unwrap());
+        monkees.reverse();
+        self.p2 = monkees[0].inspections * monkees[1].inspections;
+    }
 
     fn p1_result(&self) -> String {
         format!("{}", self.p1)
@@ -72,63 +90,64 @@ Monkey 3:
 }
 
 struct Monke {
-    inspections: u32,
-    items: Vec<u32>,
+    inspections: u128,
     option_a: usize,
     option_b: usize,
-    operation: Operation,
-    test_num: u32,
+    operation: Box<dyn Fn(&u128) -> u128>,
+    test_num: u128,
 }
 
 impl Monke {
-    fn new(monke: &str) -> Self
+    fn new(monke: &str, all_items: &mut Vec<Vec<u128>>) -> Self
     {
         let values: Vec<&str> = monke.lines().collect();
-        let items = values[1][18..].split(", ").map(|i| i.parse::<u32>().unwrap()).collect();
+        let items = values[1][18..].split(", ").map(|i| i.parse::<u128>().unwrap()).collect();
+        all_items.push(items);
         let operation_input = &values[2][19..];
-        let operation = match operation_input {
-            _square if operation_input == "old * old" => Operation::Square,
-            add if operation_input.contains("old +") => Operation::Add(add[5..].parse::<u32>().unwrap()),
-            multiply if operation_input.contains("old +") => Operation::Multiply(multiply[5..].parse::<u32>().unwrap()),
+        let operation: Box<dyn Fn(&u128) -> u128> = match operation_input {
+            _square if operation_input ==  "old * old" => {
+                // println!("1");
+                Box::new(|&num| num * num)
+            },
+            _double if operation_input == "old + old" => {
+                // println!("2");
+                Box::new(|&num| num + num)
+            } ,
+            add if operation_input.contains("old +") => {
+                // println!("3");
+                let op_number = add[6..].parse::<u128>().unwrap();
+                Box::new(move |&num| num + op_number)
+            },
+            multiply if operation_input.contains("old *") => {
+                // println!("4");
+                let op_number = multiply[6..].parse::<u128>().unwrap();
+                Box::new(move |&num| num * op_number)
+            }
             _ => panic!()
         };
-
         Self { 
             inspections: 0, 
-            items,
             operation,
             option_a: values[4][29..].parse::<usize>().unwrap(),
             option_b: values[5][30..].parse::<usize>().unwrap(),
-            test_num: values[3][21..].parse::<u32>().unwrap(),
+            test_num: values[3][21..].parse::<u128>().unwrap(),
         }
     }
 
-    fn process_items(&mut self, monkees: RefCell<Vec<Monke>>) {
-        for item in self.items.iter() {
+    fn process_item(&mut self, all_items: &mut Vec<Vec<u128>>, current: usize, p1: bool) {
+        for _i in 0..all_items[current].len() {
+            let mut item = all_items[current].pop().unwrap();
+            item =  (self.operation)(&item);
+            if p1 {
+                item = item / 3;
+            }
             self.inspections += 1;
-            if self.operation.apply(*item) / self.test_num == 0 {
-                monkees.borrow_mut()[self.option_a].items.push(*item)
+            if item % self.test_num == 0 {
+                all_items[self.option_a].push(item)
             }
             else {
-                monkees.borrow_mut()[self.option_b].items.push(*item)
+                all_items[self.option_b].push(item)
             }
-        }
-        println!("{}", self.items.len())
-    }
-}
-
-enum Operation {
-    Square,
-    Add(u32),
-    Multiply(u32)
-}
-
-impl Operation {
-    fn apply (&self, v: u32) -> u32  {
-        match self {
-            Self::Square => v * v,
-            Self::Add(n) => *n + v,
-            Self::Multiply(n) => *n * v,
         }
     }
 }
