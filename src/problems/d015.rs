fa8b38cac18097131b813dfd01518879a2f9a9ef
HakunaRumata <49195326+HakunaRumata@users.noreply.github.com>
2022-12-16 22:58:47 +0100

    started day 15

--- src/problems/d015.rs
+++ src/problems/d015.rs
@@ -1,6 +1,7 @@
 pub struct Day {
     p1: i64,
     p2: i64,
+    _input: String,
 }
 
 impl crate::Problem for Day {
@@ -8,10 +9,21 @@ impl crate::Problem for Day {
     const DAY: u32 = 15;
 
     fn new(_input: String) -> Self {
-        Self { p1: 0, p2: 0 }
+        Self {
+            p1: 0,
+            p2: 0,
+            _input,
+        }
     }
 
-    fn do_p1(&mut self) {}
+    fn do_p1(&mut self) {
+        // for line in self.input.lines() {
+        //     if let Some((s, b)) = line[10..].split_once(": closest beacon is at ") {
+        //         println!("{s}");
+        //         println!("{b}");
+        //     }
+        // }
+    }
 
     fn do_p2(&mut self) {}
 
@@ -23,3 +35,26 @@ impl crate::Problem for Day {
         format!("{}", self.p2)
     }
 }
+
+// struct Sensor{
+//     location: Point,
+//     beacon: Point,
+//     covered_areas: Vec<u32>,
+// }
+
+// impl Sensor {
+//     fn new(location: Point, beacon: Point) -> Self {
+//         Self { location, beacon, covered_areas: Vec::new() }
+//     }
+// }
+
+// struct Point {
+//     x: u32,
+//     y: u32,
+// }
+
+// impl Point {
+//     fn new(x: u32, y: u32) -> Self {
+//         Self { x, y, }
+//     }
+// }
